6902ba7a1b4364d60e05bead7ed73dc6b417ad1b
Anthony Roscoe <me@anthonyroscoe.com>
2011-01-10 16:37:11 -0500

    creating item DOM with closure

--- assets/js/system.js
+++ assets/js/system.js
@@ -1,1 +1,34 @@
-/* SYSTEM **************************************************/ 
+/* SYSTEM **************************************************/
+
+goog.provide('gtd.Item');
+goog.provide('gtd.createItems');
+
+goog.require('goog.dom');
+
+
+gtd.createItems = function(data, itemContainer){
+    var items = [];
+    for(var i=0; i<data.length; i++){
+        var item = new gtd.Item(data[i], itemContainer);
+        items.push(item)
+        item.makeItemDom();
+    }
+    return items;
+};
+
+gtd.Item = function(data, itemContainer){
+    this.content = data.content;
+    this.date = data.date;
+    this.parent = itemContainer;
+};
+
+gtd.Item.prototype.makeItemDom = function(){
+    // Create DOM structure
+    this.contentElement = goog.dom.createDom('p', null, this.content);
+    
+    // Create Item
+    var newItem = goog.dom.createDom('li', null, this.contentElement);
+    
+    // Add Item to document
+    this.parent.appendChild(newItem);
+};
