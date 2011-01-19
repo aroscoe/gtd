6ebca818f04f19032932ee619da80fc39ef20b86
Anthony Roscoe <me@anthonyroscoe.com>
2011-01-18 20:56:14 -0500

    Add Items is working

--- assets/js/system.js
+++ assets/js/system.js
@@ -4,6 +4,7 @@ goog.provide('gtd.Item');
 goog.provide('gtd.createItems');
 
 goog.require('goog.dom');
+goog.require('goog.style');
 goog.require('goog.events');
 goog.require('goog.ui.Dialog');
 
@@ -68,5 +69,55 @@ gtd.Item.prototype.deleteItem = function(e){
         if (e.key == 'delete') {
             goog.dom.removeNode(this.itemElement);
         }
+    }, false, this);
+};
+
+gtd.Item.prototype.updateItem = function(text){
+    this.content = text;
+    this.contentElement.innerText = this.content;
+};
+
+gtd.Item.prototype._removeEditItemDOM = function(){
+    // Remove edit components
+    goog.dom.removeNode(goog.dom.getElementByClass('edit-item', this.itemElement));
+    goog.dom.removeNode(goog.dom.getElementByClass('menu-edit', this.itemElement));
+    
+    // Return item contents to DOM
+    goog.style.showElement(this.contentElement, true);
+    goog.style.showElement(this.contentElement.nextSibling, true);
+};
+
+// Event Handler - Edit item
+gtd.Item.prototype.editItem = function(){
+    goog.style.showElement(this.contentElement, false);
+    goog.style.showElement(this.contentElement.nextSibling, false);
+    
+    // Create edit input
+    var editInput = goog.dom.createDom('input', {'type': 'text', 'class': 'edit-item'}, this.content);
+    goog.dom.insertSiblingBefore(editInput, this.contentElement);
+    editInput.focus();
+    
+    // Change menu items
+    var optionSave = goog.dom.createDom('a', {'href': '#'}, 'Save');
+    var menuItemSave = goog.dom.createDom('li', {'class': 'btn-save'}, optionSave);
+    var optionCancel = goog.dom.createDom('a', {'href': '#'}, 'Cancel')
+    var menuItemCancel = goog.dom.createDom('li', {'class': 'btn-cancel'}, optionCancel);
+    var editMenu = goog.dom.createDom('ul', {'class': 'menu-edit'}, menuItemSave, menuItemCancel);
+    this.contentElement.parentNode.appendChild(editMenu);
+    
+    // Listener - Cancel
+    goog.events.listen(optionCancel, goog.events.EventType.CLICK, function(e){
+        e.preventDefault();
+        this._removeEditItemDOM();
+    }, false, this);
+    
+    // Listener - Save
+    goog.events.listen(optionSave, goog.events.EventType.CLICK, function(e){
+        e.preventDefault();
+        
+        var editInput = goog.dom.getElementByClass('edit-item', this.itemElement);
+        
+        this.updateItem(editInput.value);
+        this._removeEditItemDOM();
     }, false, this);
 };
