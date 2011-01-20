572d8253fa7aee26a8e1328eaf6461e18e5d2c66
Anthony Roscoe <me@anthonyroscoe.com>
2011-01-20 14:26:15 -0500

    added names to models

--- list/models.py
+++ list/models.py
@@ -6,6 +6,9 @@ class List(models.Model):
     is_active = models.BooleanField(default=1)
     items = models.ManyToManyField('Item')
     
+    def __unicode__(self):
+        return self.name if (self.name != '') else 'Unnamed'
+    
     class Admin:
         pass
 
@@ -13,6 +16,9 @@ class Item(models.Model):
     content = models.TextField(blank=True)
     date = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=1)
+    
+    def __unicode__(self):
+        return self.content[0:25] if (self.content != '') else 'Blank'
     
     class Admin:
         pass
